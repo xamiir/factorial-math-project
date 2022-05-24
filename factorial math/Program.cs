7a787b97367d35f554d2ab44332a49ef40ea9c2b
cllaahi <cllaahicabdi04@gmail.com>
2022-05-24 19:16:31 +0300

    New project

---
+++ factorial math/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial_math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = 1, number;
+            Console.WriteLine("Enter any Number:");
+            number = int.Parse(Console.ReadLine());
+            for(int i=1; i<=number;i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("factorial of "+"("+ number +"!)"+ " is: " + fact);
+            Console.ReadLine();
+        }
+    }
+}
